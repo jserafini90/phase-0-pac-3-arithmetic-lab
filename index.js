7908c95b258fab2f7b10ccb21183e13fd6e76433
jpitts_kiheihs@yahoo.com <jpitts_kiheihs@yahoo.com>
2022-03-06 21:45:01 -0500

    commit tree e89d9d235d7a2d124963a53d360261b71f8b9fe6

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+  }
+
+function subtract() {
+} 
+
+function multiply() {
+}
+
+function divide() {
+}
+
+function add(a,b) {
+return a + b;
+}
+
+function subtract(a,b) {
+return a - b;
+}
+
+function multiply(a,b) {
+return a * b;
+}
+
+function divide(a,b) {
+return a / b;
+}
+
+// reset number 
+number = 10;
+
+function increment(number = 10) {
+  return (number += 1)
+}
+    
+    function decrement(number = 10) {
+return (number -= 1)
+    }
+
+    function makeInt(n) {
+    return parseInt(n, 10);   
+    }
+    
+    function preserveDecimal(n) {
+return parseFloat(n);   
+    }
